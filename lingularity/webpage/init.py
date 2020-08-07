ee94ba78dbc22b866a78565f102396858c3b3407
W2SV <zangenbergjanek@googlemail.com>
2020-08-07 18:32:04 +0200

    button redirection

--- lingularity/webpage/init.py
+++ lingularity/webpage/init.py
@@ -1,12 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/home')
 @app.route('/')
 def index():
-    return render_template('login_screen.html')
+    return render_template('front_page.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login_page.html')
+
+
+@app.route('/sign-up')
+def sign_up():
+    return render_template('login_page.html')
 
 
 if __name__ == '__main__':
