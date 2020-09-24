556746771830b0919587c4302d64d3f4eb0b56ca
W2SV <zangenbergjanek@googlemail.com>
2020-09-24 17:30:22 +0200

    minor change

--- lingularity/backend/utils/iterables.py
+++ lingularity/backend/utils/iterables.py
@@ -1,11 +1,9 @@
-from typing import Set, Iterable, Any, Sequence, List
+from typing import Set, Iterable, Any, List
 
 
 def none_stripped(iterable: Iterable[Any]) -> List[Any]:
     return list(filter(lambda el: el is not None, iterable))
 
 
-def iterables_intersection(nested_iterables: Sequence[Iterable[Any]]) -> Set[Any]:
-    if type(nested_iterables[0]) is not set:
-        nested_iterables = list(map(set, nested_iterables))
-    return set.intersection(*nested_iterables)  # type: ignore
+def iterables_intersection(nested_iterables: Iterable[Set[Any]]) -> Set[Any]:
+    return set.intersection(*nested_iterables)
