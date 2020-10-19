54108f6d3496b25d00b51c6fe1a94cebb52bf7fa
W2SV <zangenbergjanek@googlemail.com>
2020-10-19 17:47:27 +0200

    m

--- lingularity/backend/metadata/__init__.py
+++ lingularity/backend/metadata/__init__.py
@@ -27,14 +27,17 @@ def replacement_forenames_available_for(language: str) -> bool:
 def get_substitution_forenames_map(language: str) -> SubstitutionForenamesMap:
     """ Returns:
             replacement forename data of randomly picked country passed language
-            employed in
+            is used in
 
         Assumes previous assertion of replacement forenames being available for language """
 
     assert (countries_language_employed_in := _countries_language_employed_in(language)) is not None
     country = random.choice(_data_beset_countries(countries_language_employed_in))
     assert (substitution_forenames_map := _country_metadata[country]) is not None
+
+    # add country to forenames map
     substitution_forenames_map['country'] = country
+
     return substitution_forenames_map
 
 
