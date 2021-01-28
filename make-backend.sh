cf107506c9056342ce8ba2713577d923775d19f0
W2SV <zangenbergjanek@googlemail.com>
2021-01-28 12:37:53 +0100

    fix make-backend.sh

--- make-backend.sh
+++ make-backend.sh
@@ -6,11 +6,11 @@ cd ../backend || exit
 conda activate ./env
 make wheel
 
-cd dist || exit
+cd ./dist || exit
 dist_dir_path=$(realpath .)
 latest_backend_wheel=$(find . -type f -name "backend*" | head -n 1)
 
-cd ../frontend || exit
+cd ../../frontend || exit
 conda activate ./env
 pip uninstall -y backend
 pip install "$dist_dir_path/$latest_backend_wheel"
