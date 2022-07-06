fa41ce98e1e174867657cddefd7d27cb429eda31
Janek Zangenberg <zangenbergjanek@googlemail.com>
2022-07-06 17:37:15 +0200

    Minor change

--- frontend/src/screen/training_selection.py
+++ frontend/src/screen/training_selection.py
@@ -1,5 +1,6 @@
+from __future__ import annotations
+
 import random
-from typing import Optional
 
 import asciiplot
 from backend.src.database.user_database import UserDatabase
@@ -16,18 +17,12 @@ from frontend.src.trainer_frontends.vocable_trainer import VocableTrainerFronten
 from frontend.src.utils import output, view
 from frontend.src.utils.prompt.cancelling import QUERY_CANCELLED
 from frontend.src.utils.prompt.repetition import prompt_relentlessly
-from frontend.src.utils.view import Banner, terminal
+from frontend.src.utils.view import Banner
 
 
-@view.creator(banner=Banner('lingularity/3d-ascii', 'green'))
-@State.receiver
-def __call__(state: State, training_item_sequence_plot_data: Optional[SequencePlotData] = None) -> ReentryPoint:
-    terminal.set_title(f'{state.language} Training Selection')
-
-    if training_item_sequence_plot_data:
-        _display_training_item_sequence(training_item_sequence_plot_data)
-    else:
-        _display_constitution_query(username=state.username, language=state.language)
+@view.creator(banner=Banner('lingularity/3d-ascii', 'green'), title='Training Selection')
+def __call__(training_item_sequence_plot_data: SequencePlotData | None = None) -> ReentryPoint:
+    _render_screen(training_item_sequence_plot_data)
 
     options = _get_options()
 
@@ -38,10 +33,19 @@ def __call__(state: State, training_item_sequence_plot_data: Optional[SequencePl
     callback = options[action_selection_keyword]
 
     # instantiate frontend if selected
-    if issubclass(callback, TrainerFrontend):
-        trainer_frontend = callback()
-        return __call__(training_item_sequence_plot_data=trainer_frontend.__call__())
-    return callback
+    if isinstance(callback, ReentryPoint):
+        return callback
+    assert issubclass(callback, TrainerFrontend)
+    trainer_frontend = callback()
+    return __call__(training_item_sequence_plot_data=trainer_frontend())
+
+
+@State.receiver
+def _render_screen(training_item_sequence_plot_data: SequencePlotData | None, state: State):
+    if training_item_sequence_plot_data:
+        _display_training_item_sequence(training_item_sequence_plot_data)
+    else:
+        _display_constitution_query(username=state.username, language=state.language)
 
 
 @UserDatabase.receiver
@@ -52,7 +56,7 @@ def _get_options(user_database: UserDatabase) -> OptionCollection:
         options.append(Option('Train Vocabulary', callback=VocableTrainerFrontend, keyword='vocabulary'))
 
     options.append(Option('Add Vocabulary', callback=VocableAdderFrontend))
-    options.append(Option('Quit', callback=ReentryPoint.Exit))
+    options.append(Option('Home Screen', callback=ReentryPoint.Home))
 
     return OptionCollection(options)
 
